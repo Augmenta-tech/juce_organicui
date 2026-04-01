7750400d874c3678802d0530ad74e57621c1248d
Léon <10499605+leonvictor@users.noreply.github.com>
2026-04-01 14:54:45 +0200

    Revert "feat: OSCQuery LISTEN with initial feeback also sends feedback regarding the enabled status"

--- remotecontrol/OSCRemoteControl.cpp
+++ remotecontrol/OSCRemoteControl.cpp
@@ -829,7 +829,6 @@ void OSCRemoteControl::messageReceived(const String& id, const String& message)
 					if (sendFeedback)
 					{
 						sendOSCQueryFeedback(c);
-						sendOSCQueryStateFeedback(c);
 					}
 				}
 				else if (command == "IGNORE")
