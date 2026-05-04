Augmenta-tech/juce_organicui

commit 7103462489c2ab33f3ff15ab16e8836595453031

mass fixes !

helpers/QueuedNotifier.h
@@ -9,6 +9,8 @@
 */
 #pragma once
 
+#include <atomic>
+
 #define ENUM_LIST(...) {__VA_ARGS__}
 
 #define EVENT_NO_CHECK true
@@ -46,15 +48,18 @@ public:
 
     }
 
-	bool dropMessageOnOverflow;
+    bool dropMessageOnOverflow;
 
     virtual ~QueuedNotifier() {
+        isBeingDestroyed.store(true, std::memory_order_release);
         cancelPendingUpdate();
-		listeners.clear();
-		lastListeners.clear();
-		messageQueue.clear();
-		//cancelPendingUpdate();
-	}
+        const typename QueueLock::ScopedLockType lock(queueLock);
+        listeners.clear();
+        lastListeners.clear();
+        messageQueue.clear();
+        fifo.reset();
+        //cancelPendingUpdate();
+    }
 
 
 
@@ -65,56 +70,70 @@ public:
     };
 
     void addMessage( MessageClass * msg,bool forceSendNow = false){
+        if (msg == nullptr)
+            return;
+
+        if (isBeingDestroyed.load(std::memory_order_acquire))
+        {
+            delete msg;
+            return;
+        }
+
         if(listeners.size()==0 && lastListeners.size()==0){
             delete msg;
             return;
         }
-        forceSendNow |= juce::MessageManager::getInstance()->isThisTheMessageThread();
+
+        if (auto* mm = juce::MessageManager::getInstanceWithoutCreating())
+            forceSendNow |= mm->isThisTheMessageThread();
+
         if(forceSendNow){
             listeners.call(&Listener::newMessage,*msg);
             lastListeners.call(&Listener::newMessage,*msg);
             delete msg;
             return;
         }
-        else{
 
-            // add if we are in a decent array size
-            
-			int start1,size1,start2,size2;
-			fifo.prepareToWrite(1, start1, size1, start2, size2);
-			
-			/*
-			if(size2>0){
-				if(messageQueue.size()<maxSize){messageQueue.add(msg);}
-				else{messageQueue.set(start2,msg);}
-			}
-			*/
-			if (size1 == 0)
-			{
-				if (dropMessageOnOverflow)
-				{
-					delete msg;
-				}
-				else
-				{
-					while (size1 == 0)
-					{
-						fifo.prepareToWrite(1, start1, size1, start2, size2);
-                        juce::Thread::sleep(10);
-					}
-				}
-			}else {
-				if (messageQueue.size()<maxSize) { messageQueue.add(msg); }
-				else { messageQueue.set(start1, msg); }
-			}
+        // add if we are in a decent array size
+        int start1{},size1{},start2{},size2{};
 
-			//jassert(size2 == 0);
+        for (;;)
+        {
+            if (isBeingDestroyed.load(std::memory_order_acquire))
+            {
+                delete msg;
+                return;
+            }
 
+            {
+                const typename QueueLock::ScopedLockType lock(queueLock);
+                fifo.prepareToWrite(1, start1, size1, start2, size2);
 
-			fifo.finishedWrite (size1 + size2);
-            triggerAsyncUpdate();
+                if (size1 > 0)
+                {
+                    if (messageQueue.size() <= start1)
+                    {
+                        while (messageQueue.size() <= start1)
+                            messageQueue.add(nullptr);
+                    }
+
+                    messageQueue.set(start1, msg);
+
+                    fifo.finishedWrite (size1 + size2);
+                    break;
+                }
+            }
+
+            if (dropMessageOnOverflow)
+            {
+                delete msg;
+                return;
+            }
+
+            juce::Thread::sleep(10);
         }
 
+        triggerAsyncUpdate();
     }
 
     // allow to stack all values or get oly last updated value
@@ -126,31 +145,80 @@ private:
 
     void handleAsyncUpdate() override
     {
-            int start1,size1,start2,size2;
+        if (isBeingDestroyed.load(std::memory_order_acquire))
+            return;
+
+        juce::Array<MessageClass*> messagesToDeliver;
+        MessageClass* lastMessage = nullptr;
+
+        {
+            const typename QueueLock::ScopedLockType lock(queueLock);
+
+            int start1{},size1{},start2{},size2{};
             fifo.prepareToRead(fifo.getNumReady(), start1, size1, start2, size2);
 
-                for(int i = start1 ;i <start1+ size1 ; ++i){
-                    listeners.call(&Listener::newMessage,*messageQueue.getUnchecked(i));
+            bool outOfRange = false;
+
+            for(int i = start1 ;i <start1+ size1 ; ++i)
+            {
+                if ((unsigned) i >= (unsigned) messageQueue.size())
+                {
+                    outOfRange = true;
+                    break;
                 }
 
-            for(int i = start2 ;i <start2+ size2 ; ++i){
-                listeners.call(&Listener::newMessage,*messageQueue.getUnchecked(i));
+                if (auto* message = messageQueue.getUnchecked(i))
+                {
+                    messagesToDeliver.add(message);
+                    lastMessage = message;
+                }
             }
 
-            if(size2>0)
-                lastListeners.call(&Listener::newMessage,*messageQueue.getUnchecked(start2+size2-1));
-            else if(size1>0)
-                lastListeners.call(&Listener::newMessage,*messageQueue.getUnchecked(start1+size1-1));
+            if (! outOfRange)
+            {
+                for(int i = start2 ;i <start2+ size2 ; ++i)
+                {
+                    if ((unsigned) i >= (unsigned) messageQueue.size())
+                    {
+                        outOfRange = true;
+                        break;
+                    }
+
+                    if (auto* message = messageQueue.getUnchecked(i))
+                    {
+                        messagesToDeliver.add(message);
+                        lastMessage = message;
+                    }
+                }
+            }
+
+            if (outOfRange)
+            {
+                fifo.reset();
+                return;
+            }
 
             fifo.finishedRead(size1 + size2);
+        }
+
+        for (auto* message : messagesToDeliver)
+            listeners.call(&Listener::newMessage,*message);
+
+        if (lastMessage != nullptr)
+            lastListeners.call(&Listener::newMessage,*lastMessage);
     }
 
 
 
+    using QueueLock = CriticalSectionToUse;
+
     juce::AbstractFifo fifo;
     int maxSize;
     juce::OwnedArray<MessageClass,CriticalSectionToUse> messageQueue;
+    QueueLock queueLock;
+    std::atomic<bool> isBeingDestroyed { false };
 
-    juce::ListenerList<Listener/*, juce::Array<Listener*, juce::CriticalSection>*/> listeners;
-    juce::ListenerList<Listener/*, juce::Array<Listener*, juce::CriticalSection>*/> lastListeners;
+    using ThreadSafeListenerArray = juce::Array<Listener*, CriticalSectionToUse>;
+    juce::ListenerList<Listener, ThreadSafeListenerArray> listeners;
+    juce::ListenerList<Listener, ThreadSafeListenerArray> lastListeners;
 };

controllable/ControllableContainer.cpp
@@ -1291,6 +1291,7 @@ var ControllableContainer::selectFromScript(const var::NativeFunctionArgs& a)
 var ControllableContainer::addTriggerFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 2)) return var();
 
 	Controllable* c = cc->getControllableByName(args.arguments[0], true, false);
@@ -1302,6 +1303,7 @@ var ControllableContainer::addTriggerFromScript(const var::NativeFunctionArgs& a
 var ControllableContainer::addBoolParameterFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 3)) return var();
 
 	Controllable* c = cc->getControllableByName(args.arguments[0], true, false);
@@ -1315,6 +1317,7 @@ var ControllableContainer::addBoolParameterFromScript(const var::NativeFunctionA
 var ControllableContainer::addIntParameterFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 3)) return var();
 
 	Controllable* c = cc->getControllableByName(args.arguments[0], true, false);
@@ -1328,6 +1331,7 @@ var ControllableContainer::addIntParameterFromScript(const var::NativeFunctionAr
 var ControllableContainer::addFloatParameterFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 3)) return var();
 
 	Controllable* c = cc->getControllableByName(args.arguments[0], true, false);
@@ -1341,6 +1345,7 @@ var ControllableContainer::addFloatParameterFromScript(const var::NativeFunction
 var ControllableContainer::addStringParameterFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 3)) return var();
 
 	Controllable* c = cc->getControllableByName(args.arguments[0], true, false);
@@ -1354,6 +1359,7 @@ var ControllableContainer::addStringParameterFromScript(const var::NativeFunctio
 var ControllableContainer::addEnumParameterFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 2)) return var();
 
 	Controllable* c = cc->getControllableByName(args.arguments[0], true, false);
@@ -1374,6 +1380,7 @@ var ControllableContainer::addEnumParameterFromScript(const var::NativeFunctionA
 var ControllableContainer::addTargetParameterFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 2)) return var();
 
 	Controllable* c = cc->getControllableByName(args.arguments[0], true, false);
@@ -1393,6 +1400,7 @@ var ControllableContainer::addTargetParameterFromScript(const var::NativeFunctio
 var ControllableContainer::addColorParameterFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 3)) return var();
 
 	Controllable* c = cc->getControllableByName(args.arguments[0], true, false);
@@ -1428,6 +1436,7 @@ var ControllableContainer::addColorParameterFromScript(const var::NativeFunction
 var ControllableContainer::addPoint2DParameterFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 2)) return var();
 
 	Controllable* c = cc->getControllableByName(args.arguments[0], true, false);
@@ -1441,6 +1450,7 @@ var ControllableContainer::addPoint2DParameterFromScript(const var::NativeFuncti
 var ControllableContainer::addPoint3DParameterFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 2)) return var();
 
 	Controllable* c = cc->getControllableByName(args.arguments[0], true, false);
@@ -1454,6 +1464,7 @@ var ControllableContainer::addPoint3DParameterFromScript(const var::NativeFuncti
 var ControllableContainer::addFileParameterFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 2)) return var();
 
 	Controllable* c = cc->getControllableByName(args.arguments[0], true, false);
@@ -1468,6 +1479,7 @@ var ControllableContainer::addFileParameterFromScript(const var::NativeFunctionA
 var ControllableContainer::addAutomationFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 1)) return var();
 
 	Automation* a = new Automation(args.arguments[0].toString(), nullptr, false);
@@ -1479,6 +1491,7 @@ var ControllableContainer::addAutomationFromScript(const var::NativeFunctionArgs
 var ControllableContainer::addContainerFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 1)) return var();
 
 	ControllableContainer* newCC = cc->getControllableContainerByName(args.arguments[0], true, false);
@@ -1493,6 +1506,7 @@ var ControllableContainer::addContainerFromScript(const var::NativeFunctionArgs&
 var ControllableContainer::removeContainerFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 1)) return var();
 
 	ControllableContainer* removeCC = cc->getControllableContainerByName(args.arguments[0], true, false);
@@ -1505,6 +1519,7 @@ var ControllableContainer::removeContainerFromScript(const var::NativeFunctionAr
 var ControllableContainer::removeControllableFromScript(const var::NativeFunctionArgs& args)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, args, 1)) return var();
 
 	Controllable* c = cc->getControllableByName(args.arguments[0], true, false);
@@ -1552,6 +1567,7 @@ var ControllableContainer::getControlAddressFromScript(const var::NativeFunction
 var ControllableContainer::getScriptControlAddressFromScript(const var::NativeFunctionArgs& a)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(a);
+	if (cc == nullptr) return var();
 	return "root" + cc->getControlAddress().replaceCharacter('/', '.');
 }
 
@@ -1559,6 +1575,7 @@ var ControllableContainer::getContainersFromScript(const var::NativeFunctionArgs
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
 
+	if (cc == nullptr) return var();
 	if (cc->isBeingDestroyed)
 	{
 		LOGWARNING("Get containers from script, container is being destroyed or badly referenced");
@@ -1578,6 +1595,7 @@ var ControllableContainer::getControllablesFromScript(const var::NativeFunctionA
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(args);
 
+	if (cc == nullptr) return var();
 	if (cc->isBeingDestroyed)
 	{
 		LOGWARNING("Get containers from script, container is being destroyed or badly referenced");
@@ -1604,12 +1622,14 @@ var ControllableContainer::getControllablesFromScript(const var::NativeFunctionA
 var ControllableContainer::getJSONDataFromScript(const var::NativeFunctionArgs& a)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(a);
+	if (cc == nullptr) return var();
 	return cc->getJSONData(a.numArguments > 0 ? (bool)(int)a.arguments[0] : false);
 }
 
 var ControllableContainer::loadJSONDataFromScript(const var::NativeFunctionArgs& a)
 {
 	ControllableContainer* cc = getObjectFromJS<ControllableContainer>(a);
+	if (cc == nullptr) return var();
 	if (!checkNumArgs(cc->niceName, a, 1)) return false;
 	cc->loadJSONData(a.arguments[0], a.numArguments > 1 ? (bool)(int)a.arguments[1] : false);
 	return true;

controllable/ControllableContainer.h
@@ -33,7 +33,7 @@ public:
 
 	bool nameCanBeChangedByUser;
 
-	bool isClearing;
+	std::atomic<bool>  isClearing;
 
 	//Editor
 	bool canInspectChildContainers;

controllable/parameter/Parameter.cpp
@@ -435,6 +435,22 @@ void Parameter::notifyValueChanged() {
 
 	if (!lock.isLocked()) return;
 
+	if (auto* mm = MessageManager::getInstanceWithoutCreating(); mm != nullptr && !mm->isThisTheMessageThread())
+	{
+		WeakReference<Parameter> safeThis(this);
+		const auto valueCopy = getValue();
+
+		MessageManager::callAsync([safeThis, valueCopy]()
+		{
+			if (safeThis == nullptr || safeThis->isBeingDestroyed) return;
+
+			safeThis->parameterListeners.call(&ParameterListener::parameterValueChanged, safeThis.get());
+			safeThis->queuedNotifier.addMessage(new ParameterEvent(ParameterEvent::VALUE_CHANGED, safeThis.get(), valueCopy));
+		});
+
+		return;
+	}
+
 	parameterListeners.call(&ParameterListener::parameterValueChanged, this);
 	queuedNotifier.addMessage(new ParameterEvent(ParameterEvent::VALUE_CHANGED, this, getValue()));
 	//isNotifyingChange = false;

engine/EngineFileDocument.cpp
@@ -247,7 +247,7 @@ juce::Result Engine::saveCopy()
 Result Engine::saveBackupDocument(int index)
 {
 
-	if (GlobalSettings::getInstance()->autoSaveOnChangeOnly->boolValue() && !isThereChangeToBackup ) {return Result::ok();}
+	if (GlobalSettings::getInstance()->autoSaveOnChangeOnly->boolValue() && !isThereChangeToBackup) { return Result::ok(); }
 
 	if (!getFile().existsAsFile()) return Result::ok();
 
@@ -338,8 +338,8 @@ File Engine::getAutosavesDirectory(const File& originalFile) const
 {
 	File f = originalFile;
 	String curFileName;
-	File curFileFolder; 
-	if(f.existsAsFile())
+	File curFileFolder;
+	if (f.existsAsFile())
 	{
 		curFileName = originalFile.getFileNameWithoutExtension();
 		curFileFolder = originalFile.getParentDirectory();
@@ -469,6 +469,12 @@ void Engine::loadJSONData(var data, ProgressTask* loadingTask)
 
 
 	DynamicObject* md = data.getDynamicObject()->getProperty("metaData").getDynamicObject();
+	if (md == nullptr)
+	{
+		LOGERROR("File format error : no metaData found");
+		setFile(File());
+		return;
+	}
 	bool versionChecked = checkFileVersion(md);
 
 	String versionString = md->hasProperty("version") ? md->getProperty("version").toString() : "?";
@@ -623,6 +629,7 @@ void Engine::loadJSONDataEngine(var data, ProgressTask* loadingTask)
 
 bool Engine::checkFileVersion(DynamicObject* metaData, bool checkForNewerVersion)
 {
+	if (metaData == nullptr) return false;
 	if (!metaData->hasProperty("version")) return false;
 	String versionToCheck = checkForNewerVersion ? getAppVersion() : getMinimumRequiredFileVersion();
 	//DBG(metaData->getProperty("version").toString() << " / " << versionToCheck);

inspectable/Inspectable.h
@@ -47,7 +47,7 @@ public:
 
 	//for multiSelection
 	bool isPreselected;
-	bool isBeingDestroyed;
+	std::atomic<bool> isBeingDestroyed;
 
 	bool highlightLinkedInspectableOnSelect;
 	juce::Array<juce::WeakReference<Inspectable>> linkedInspectables;

manager/BaseManager.h
@@ -1033,6 +1033,7 @@ template<class T>
 juce::var BaseManager<T>::addItemFromScript(const juce::var::NativeFunctionArgs& args)
 {
 	BaseManager<T>* m = getObjectFromJS<BaseManager<T>>(args);
+	if (m == nullptr || m->isBeingDestroyed) return juce::var();
 
 	//if (args.numArguments) return juce::var(); 
 	if (m->managerFactory == nullptr || m->managerFactory->defs.size() == 1)

commit e291fb620bdef1a4fcecc0d05d33a9a2f49af1a8

Fix crashes happening when destroying a QueuedNotifier introduced in 7103462489c2ab33f3ff15ab16e8836595453031

helpers/QueuedNotifier.h
@@ -137,9 +137,30 @@ public:
     }
 
     // allow to stack all values or get oly last updated value
-    void addListener(Listener* newListener) { listeners.add(newListener); }
-    void addAsyncCoalescedListener(Listener* newListener) { lastListeners.add(newListener); }
-    void removeListener(Listener* listener) { listeners.remove(listener);lastListeners.remove(listener); }
+    void addListener(Listener* newListener) 
+    {
+		if (isBeingDestroyed.load(std::memory_order_acquire))
+			return;
+        
+        listeners.add(newListener); 
+    }
+
+    void addAsyncCoalescedListener(Listener* newListener) 
+    {
+		if (isBeingDestroyed.load(std::memory_order_acquire))
+			return;
+		
+        lastListeners.add(newListener);
+	}
+
+    void removeListener(Listener* listener) 
+    { 
+        if (isBeingDestroyed.load(std::memory_order_acquire))
+			return;
+        
+        listeners.remove(listener);
+        lastListeners.remove(listener); 
+    }
 
     void clearQueue()
     {